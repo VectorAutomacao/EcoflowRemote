ddd2f94e4370806bc01a7c3b590bfd93d19cadf3
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-05-05 17:57:25 -0300

    Implementado set count e get count Versão funcional

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -70,7 +70,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("ECOflow v0.3");
+        setTitle("ECOflow v0.4");
         setIconImages(null);
         setName("framePrincipal"); // NOI18N
         setResizable(false);
